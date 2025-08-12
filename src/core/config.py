e0f3ae469f5725c2434ec6659bb0729d1770fe6b
Yiorgos Marinellis <mari.giorgos@gmail.com>
2025-08-12 11:49:26 +0300

    Add default daily, hourly history variables

--- src/core/config.py
+++ src/core/config.py
@@ -19,8 +19,37 @@ DATABASE_NAME = os.environ.get('WEATHER_SRV_DATABASE_NAME', 'openagridb')
 OPENWEATHERMAP_API_KEY = os.environ.get('WEATHER_SRV_OPENWEATHERMAP_API_KEY', '')
 HISTORY_WEATHER_PROVIDER = os.environ.get('HISTORY_WEATHER_PROVIDER', 'openmeteo')
 OM_CACHE_VARIABLES = {
-    "daily": ["temperature_2m_max", "temperature_2m_min"],
-    "hourly": ["temperature_2m", "relative_humidity_2m"]
+    "daily": [
+        "temperature_2m_min",
+        "temperature_2m_max",
+        "temperature_2m_mean",
+        "precipitation_sum",
+        "rain_sum",
+        "wind_speed_10m_max",
+        "wind_gusts_10m_max",
+        "wind_direction_10m_dominant"
+    ],
+    "hourly": [
+        "temperature_2m",
+        "relative_humidity_2m",
+        "precipitation",
+        "rain",
+        "pressure_msl",
+        "surface_pressure",
+        "cloud_cover",
+        "et0_fao_evapotranspiration",
+        "wind_speed_10m",
+        "wind_direction_10m",
+        "wind_gusts_10m",
+        "soil_temperature_0_to_7cm",
+        "soil_temperature_7_to_28cm",
+        "soil_temperature_28_to_100cm",
+        "soil_temperature_100_to_255cm",
+        "soil_moisture_0_to_7cm",
+        "soil_moisture_7_to_28cm",
+        "soil_moisture_28_to_100cm",
+        "soil_moisture_100_to_255cm"
+    ]
 }
 
 # ALLOWED_HOSTS
